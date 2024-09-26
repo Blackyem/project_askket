20b004867b0126a68a30b0db00b8049b9a62b7e9
Oladipupo <kolayem44@gmail.com>
2024-09-26 15:50:41 +0100

    amend

--- app/update-askket/page.jsx
+++ app/update-askket/page.jsx
@@ -1,12 +1,13 @@
 
     "use client"
   
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "/components/Form"
  
-const EditAskket = () => { 
+const UpdateAskket = () => { 
+  
   const router = useRouter();
   const searchParams = useSearchParams(); 
   const askketId = searchParams.get("id");  
@@ -16,47 +17,48 @@ const EditAskket = () => {
 
 
   useEffect(() => {
-   const getAskketDetails = async () => {
-     const response = await fetch(`/api/askket/${askketId}`)
-     const data = await response.json();  
-
-    setPost({
-       askket: data.askket,
-       tag: data.tag,
-    }) 
-   } 
-
-   if(askketId) getAskketDetails()
- }, [askketId])
+    const getAskketDetails = async () => {
+      const response = await fetch(`/api/askket/${askketId}`)
+      const data = await response.json();  
+      
+      setPost({
+        askket: data.askket,
+        tag: data.tag,
+      }) 
+    } 
+    
+    if(askketId) getAskketDetails()
+    }, [askketId])
 
    
    const updateAskket = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-   if(!askketId) return alert("Askket ID not found")
-
-     try { 
-      const response = await fetch(`/api/askket/${askketId}`, {
-        method: "PATCH",  
-        body: JSON.stringify({ 
-          askket: post.askket,
-          tag: post.tag
-        }),
-      })
-
-      if(response.ok) {
-        router.push("/"); 
+     e.preventDefault();
+     setIsSubmitting(true);
+     
+     if(!askketId) return alert("Askket ID not found")
+      
+      try { 
+        const response = await fetch(`/api/askket/${askketId}`, {
+          method: "PATCH",  
+          body: JSON.stringify({ 
+            askket: post.askket,
+            tag: post.tag
+          }),
+        })
+        
+        if(response.ok) {
+          router.push("/"); 
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsSubmitting(false)
       }
-     } catch (error) {
-       console.log(error);
-     } finally {
-      setIsSubmitting(false)
-     }
-
-   };
+      
+    };
  
   return (
+    <Suspense> 
       <Form 
         type="Edit"
         post={post}
@@ -64,18 +66,19 @@ const EditAskket = () => {
         submitting={submitting}
         handleSubmit={updateAskket}
         />
-   
+    </Suspense>
     ) 
   }
-
+  
 
   const Page = () => {
     return (
         <Suspense>
-            <EditAskket />
+            <UpdateAskket />
         </Suspense>
     )
 }
 
+  
  
 export default Page;
